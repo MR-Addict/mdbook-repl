1c983f24421f72f46e836e994468e1aa57080690
Cael <MR-Addict@qq.com>
2024-02-21 14:02:14 +0800

    ci: build frontend with custom baseurl

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,8 @@ import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-function getBaseUrl() {
-  if (!process.env.GITHUB_REPOSITORY) return "/";
-  const [githubOwner, repository] = process.env.GITHUB_REPOSITORY.split("/");
-  return `https://${githubOwner}.github.io/${repository}/embed`;
-}
-
 export default defineConfig({
-  base: getBaseUrl(),
+  base: process.env.BASE_URL || "/",
   plugins: [million.vite({ auto: true }), react()],
   resolve: { alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }] }
 });
