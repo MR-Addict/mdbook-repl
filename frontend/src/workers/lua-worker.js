e7ad95f33474f17bf1918593aaf14b0f0cff92f2
Cael <MR-Addict@qq.com>
2026-03-18 08:41:15 +0800

    refactor: improve formatting and readability of lua-worker.js

--- frontend/src/workers/lua-worker.js
+++ frontend/src/workers/lua-worker.js
@@ -5,7 +5,9 @@ const factory = new wasmoon.LuaFactory();
 const postmessage = (status, msg) => self.postMessage({ lang: "lua", output: { status, data: msg } });
 
 const stderr = (...msg) => postmessage("running", [{ color: "red", msg }]);
-const stdout = (...msg) => { if (msg.length) postmessage("running", [{ color: "normal", msg }]);};
+const stdout = (...msg) => {
+  if (msg.length) postmessage("running", [{ color: "normal", msg }]);
+};
 
 async function waitWasmoonReady() {
   postmessage("idle", []);
@@ -21,37 +23,41 @@ self.onmessage = async (event) => {
   // add all the context to the worker
   for (const key of Object.keys(context)) self[key] = context[key];
 
-  const lua = await factory.createEngine()
-  try{
-    lua.global.set('print', wasmoon.decorateFunction((thread, argsQuantity) => {
-        const values = [];
-        for (let i = 1; i <= argsQuantity; i++) {
+  const lua = await factory.createEngine();
+  try {
+    lua.global.set(
+      "print",
+      wasmoon.decorateFunction(
+        (thread, argsQuantity) => {
+          const values = [];
+          for (let i = 1; i <= argsQuantity; i++) {
             values.push(thread.indexToString(i));
+          }
+          stdout(...values.map(String));
+        },
+        {
+          receiveArgsQuantity: true,
+          receiveThread: true
         }
-        stdout(...values.map(String));
-    }, {
-        receiveArgsQuantity: true,
-        receiveThread: true
-    }));
+      )
+    );
 
-    await lua.global.set('error', (...args) => {
+    await lua.global.set("error", (...args) => {
       stderr(...args.map(String));
     });
 
     if (!code) postmessage("finished", [{ color: "red", msg: "lua code is empty" }]);
     else {
       try {
-          await lua.doString(code);
-          postmessage("finished", []);
+        await lua.doString(code);
+        postmessage("finished", []);
       } catch (err) {
-          postmessage("finished", [{ color: "red", msg: [err.message] }]);
+        postmessage("finished", [{ color: "red", msg: [err.message] }]);
       }
     }
-  }
-  catch(e){
+  } catch (e) {
     stderr(e.toString());
-  }
-  finally{
+  } finally {
     lua.global.close();
     postmessage("finished", []);
   }
